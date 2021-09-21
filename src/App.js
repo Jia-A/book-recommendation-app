b234499eeed802b50f570437bd587f0328725851
Jiya Agrawal <agrawaljiya2011@gmail.com>
2021-09-21 13:36:12 +0530

    description-added

--- src/App.js
+++ src/App.js
@@ -5,36 +5,86 @@ var books = {
   mystery: [
     {
       name: "Verity",
-      rating: "4/5"
+      description : "You never know what the reality is until you reach the end!!",
+      rating: "7/10"
     },
     {
       name: "The girl on the train",
-      rating: "4/5"
+      description:"This book will not let you sleep until you complete it.",
+      rating: "8/10"
     },
     {
       name: "Into the water",
-      rating : "3.5/5"
+      description:"The last sentence will reveal all the secrets!",
+      rating : "8.5/10"
     }
   ],
   calm: [
-    { name: "The perks of being a wallflower", rating: "4.5/5" },
-    { name: "Five people you meet in heaver", rating: "3/5" },
-    { name: "The goldfinch", rating: "4.5/5"}
+    { 
+      name: "The perks of being a wallflower", 
+    description: "You'll see yourself in this one if you over-think and over-analyze!",
+    rating: "8/10" 
+    },
+    {
+      name: "Five people you meet in heaver", 
+    description: "This book gives you 5 important life-lessons",
+    rating: "8/10" 
+    },
+    { 
+      name: "The goldfinch", 
+    description: "That one painting which became his last home",
+    rating: "9/10"
+    }
   ],
   must_read_once: [
-    { name: "Kafka on the shore", rating: "3/5" },
-    { name: "Everything Everything", rating: "4/5" },
-    { name: "They both die at the end", rating:"3.5/5"}
+    { 
+      name: "Kafka on the shore", 
+    description: "This book has imagination, supernatures, time and a rock!",
+    rating: "9/10" 
+    },
+    { 
+      name: "Everything Everything", 
+    description: "What's life without a little bit of risk?",
+    rating: "7/10" 
+    },
+    { 
+      name: "They both die at the end", 
+    description: "A call and everything is changed.",
+    rating:"8/10"
+    }
   ],
   heart_wrenching: [
-    { name: "The boy in the striped pyjamas", rating: "5/5" },
-    { name: "A thousad splendid suns", rating: "4.5/5" },
-    { name: "And the mountains echoed", rating:"5/5"}
+    { 
+      name: "The boy in the striped pyjamas", 
+    description: "Two boys, the mirror of each other, sitting either side of a wire fence.",
+    rating: "10/10" 
+    },
+    { 
+      name: "A thousad splendid suns", 
+    description : "Two women, same yet different.",
+    rating: "10/10" 
+    },
+    { name: "And the mountains echoed", 
+    description : "Their fate was written somewhere else.",
+    rating:"10/10"
+    }
   ],
   teen_novels: [
-    { name: "Confess", rating: "4/5" },
-    { name: "Hopeless", rating: "4.5/5" },
-    { name: "Paper towns", rating:"4.5/5"}
+    {
+      name: "Confess", 
+    description : "The confession could be more destructive than the actual sin.",
+    rating: "7/10" 
+    },
+    { 
+      name: "Hopeless", 
+    description : "This one will have you in tears because of it's delicacy.",
+    rating: "10/10" 
+    },
+    { 
+      name: "Paper towns", 
+    description : "The clues lead him to her but they were there for something else.",
+    rating:"9/10"
+    }
   ],
   
 };
@@ -90,6 +140,7 @@ export default function App() {
             >
               {" "}
               <div style={{ fontSize: "larger" }}>{result.name}</div>
+              <div style={{ fontSize: "larger" }}>{result.description}</div>
               <div style={{ fontSize: "larger" }}>{result.rating}</div>
             </li>
           ))}
